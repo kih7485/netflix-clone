d17c4254f756398c48f4fa9990c0d5c35aafd4e2
inhan <korea4127@gmail.com>
2022-06-12 00:27:33 +0900

    헤더 추가

---
+++ components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <div className='flex items-center space-x-2'>
+                <img
+                    src="https://rb.gy/ulxxee"
+                    width={100}
+                    height={100}
+                    className="object-contain cursor-pointer"
+                />
+                <ul>
+                    <li>홈으로</li>
+                    <li>드라마</li>
+                    <li>영화</li>
+                    <li>신작 & 인기</li>
+                    <li>즐겨찾기</li>
+                </ul>
+            </div>
+           
+        </header>
+  )
+}
+
+export default Header
