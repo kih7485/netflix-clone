d3ef72939a9aafba66d69365b89f7bc59fb54c0d
inhan <korea4127@gmail.com>
2022-07-10 23:52:00 +0900

    구독 테이블 완료

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -2,75 +2,106 @@ import { CheckIcon } from '@heroicons/react/outline'
 import { Product } from '@stripe/firestore-stripe-payments'
 import Head from 'next/head'
 import Link from 'next/link'
-import React from 'react'
+import { useState } from 'react'
 import useAuth from '../hooks/useAuth'
+import { loadCheckout } from '../lib/stripe'
 import Table from './Table'
+// import Loader from './Loader'
 
-interface Props{
-    products: Product[]
+interface Props {
+  products: Product[]
 }
 
-function Plans({products}:Props) {
-    const { logout, user } = useAuth()
-  
-    return (
-      
-        <div>
-            <Head>
-                <title>넷플릭스 클론코딩</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <header className="border-b border-white/10 bg-[#141414]">
-                <Link href="/">
-                    <img
-                        src="https://rb.gy/ulxxee"
-                        alt="Netflix"
-                        width={150}
-                        height={90}
-                        className="object-contain cursor-pointer"
-                    />
-                </Link>
-                <button
-                    className="text-lg font-medium hover:underline"
-                    onClick={logout}
-                >
-                로그아웃
-                </button>
-            </header>
-            <main className="max-w-5xl px-5 pb-12 mx-auto transition-all pt-28 md:px-10">
-                <h1 className="mb-3 text-3xl font-medium">
-                    Choose the plan that's right for you
-                </h1>
-                <ul>
-                    <li className="flex items-center text-lg gap-x-2">
-                        <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
-                        Ad-free.
-                    </li>
-                    <li className="flex items-center text-lg gap-x-2">
-                        <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
-                        just for you.
-                    </li>
-                    <li className="flex items-center text-lg gap-x-2">
-                        <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
-                        your plan anytime.
-                    </li>
-                </ul>
-                <div className='flex flex-col mt-4 space-y-4'>
-                    <div className='flex items-center self-end justify-end w-full md:w-3/5'>
-                        {products.map((product) => (
-                            <div key={product.id } className='planBox'>
-                                {product.name}
-                            </div>
-                        ))}
-                    </div>
+function Plans({ products }: Props) {
+  const { logout, user } = useAuth()
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2])
+  const [isBillingLoading, setBillingLoading] = useState(false)
 
-                    <Table products={products}/>
-                    <button> 
-                        구독
-                    </button>
-                </div>
-            </main>
+  console.log(products)
+
+  const subscribeToPlan = () => {
+    if (!user) return
+
+    loadCheckout(selectedPlan?.prices[0].id!)
+    setBillingLoading(true)
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="border-b border-white/10 bg-[#141414]">
+        <Link href="/">
+          <img
+            src="https://rb.gy/ulxxee"
+            alt="Netflix"
+            width={150}
+            height={90}
+            className="object-contain cursor-pointer"
+          />
+        </Link>
+        <button
+          className="text-lg font-medium hover:underline"
+          onClick={logout}
+        >
+          Sign Out
+        </button>
+      </header>
+
+      <main className="max-w-5xl px-5 pb-12 mx-auto transition-all pt-28 md:px-10">
+        <h1 className="mb-3 text-3xl font-medium">
+          Choose the plan that's right for you
+        </h1>
+        <ul>
+          <li className="flex items-center text-lg gap-x-2">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Watch all you want.
+            Ad-free.
+          </li>
+          <li className="flex items-center text-lg gap-x-2">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Recommendations
+            just for you.
+          </li>
+          <li className="flex items-center text-lg gap-x-2">
+            <CheckIcon className="h-7 w-7 text-[#E50914]" /> Change or cancel
+            your plan anytime.
+          </li>
+        </ul>
+
+        <div className="flex flex-col mt-4 space-y-4">
+          <div className="flex items-center self-end justify-end w-full md:w-3/5">
+            {products.map((product) => (
+              <div
+                className={`planBox ${
+                  selectedPlan?.id === product.id ? 'opacity-100' : 'opacity-60'
+                }`}
+                key={product.id}
+                onClick={() => setSelectedPlan(product)}
+              >
+                {product.name}
+              </div>
+            ))}
+                  </div>
+                  <Table products={products} selectedPlan={selectedPlan} />
+
+          <button
+            disabled={!selectedPlan || isBillingLoading}
+            className={`mx-auto w-11/12 rounded bg-[#E50914] py-4 text-xl shadow hover:bg-[#f6121d] md:w-[420px] ${
+              isBillingLoading && 'opacity-60'
+            }`}
+            onClick={subscribeToPlan}
+          >
+                      {isBillingLoading ? (
+                          <div/>
+            //   <Loader color="dark:fill-gray-300" />
+            ) : (
+              'Subscribe'
+            )}
+          </button>
         </div>
+      </main>
+    </div>
   )
 }
 
--- components/Table.tsx
+++ components/Table.tsx
@@ -1,33 +1,82 @@
-import { Product } from '@stripe/firestore-stripe-payments'
-import React from 'react'
+import { CheckIcon } from '@heroicons/react/outline';
+import { Product } from '@stripe/firestore-stripe-payments';
 
-interface Props{
-    products: Product[]
+interface Props {
+  products: Product[]
+  selectedPlan: Product | null
 }
 
-function Table({products}: Props) {
+function Table({ products, selectedPlan }: Props) {
   return (
-      <table>
-          <tbody className='divide-y divide-[gray]'>
-              <tr className='tableRow'>
-                  <td>월 요금</td>
-                  {products.map((product) => (
-                      <td key={product.id} className='tableDataFeature'>
-                          {product.prices[0].unit_amount!} 원
-                      </td>
-                  ))}
-              </tr> 
-              <tr className='tableRow'>
-                  <td>화질</td>
-                  {products.map((product) => (
-                      <td key={product.id} className='tableDataFeature'>
-                          {product.metadata.videoQuality}
-                      </td>
-                  ))}
-              </tr> 
-          </tbody>
-      </table>
+    <table>
+      <tbody className="divide-y divide-[gray]">
+        <tr className="tableRow">
+          <td className="tableDataTitle">월 구독료</td>
+          {products.map((product) => (
+            <td
+              className={`tableDataFeature ${
+                selectedPlan?.id === product.id
+                  ? 'text-[white]'
+                  : 'text-[gray]'
+              }`}
+              key={product.id}
+            >
+              {product.prices[0].unit_amount!} 원
+            </td>
+          ))}
+        </tr>
+        <tr className="tableRow">
+          <td className="tableDataTitle">영상 퀄리티</td>
+          {products.map((product) => (
+            <td
+              className={`tableDataFeature ${
+                selectedPlan?.id === product.id
+                  ? 'text-[white]'
+                  : 'text-[gray]'
+              }`}
+              key={product.id}
+            >
+              {product.metadata.videoQuality}
+            </td>
+          ))}
+        </tr>
+        <tr className="tableRow">
+          <td className="tableDataTitle">화질</td> 
+          {products.map((product) => (
+            <td
+              className={`tableDataFeature ${
+                selectedPlan?.id === product.id
+                  ? 'text-[white]'
+                  : 'text-[gray]'
+              }`}
+              key={product.id}
+            >
+              {product.metadata.resolution}
+            </td>
+          ))}
+        </tr>
+        <tr className="tableRow">
+          <td className="tableDataTitle">
+            Watch on your TV, computer, mobile phone and tablet
+          </td>
+          {products.map((product) => ( 
+            <td
+              className={`tableDataFeature ${
+                selectedPlan?.id === product.id
+                  ? 'text-[#E50914]'
+                  : 'text-[gray]'
+              }`}
+              key={product.id}
+            >
+              {product.metadata.portability === 'true' && (
+                <CheckIcon className="inline-block w-8 h-8" />
+              )}
+            </td>
+          ))}
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
-export default Table
+export default Table;
